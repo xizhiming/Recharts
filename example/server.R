85a77dbad5b18a02f74d2d70b78b6dad74913a90
xizhiming <15800638740@163.com>
2016-05-18 16:25:30 +0800

    first commit

--- example/server.R
+++ example/server.R
@@ -6,7 +6,7 @@ shinyServer(
                                 '中间'=c(25,35,30,25,45),
                                 '最低'=c(15,25,20,15,35))
     row.names(data_line_bar) <- c('3-1','3-2','3-3','3-4','3-5')
-    
+
     data_line_bar_2 <- data.frame('最高'=c(35,45,40,35,55))
     row.names(data_line_bar_2) <- c('3-1','3-2','3-3','3-4','3-5')
     #饼图数据
@@ -17,15 +17,15 @@ shinyServer(
     )
     #漏斗图数据
     data_funnel <- data.frame('点击'=100,
-                              '下单_111112'=50,
-                              '支付1111'=20,
-                              '收货565'=19)
+                              '下单'=50,
+                              '支付'=20,
+                              '收货'=19)
     #地图数据
     data_map_china <- data.frame("上海"=1200,"北京"=500)
     data_map_BJ <- data.frame("黄浦区"=120,"浦东新区"=300)
-    
+
     output$data_line_bar_1 <- renderEplot({
-      Eplot(type="line",data=data_line_bar,width='600px',height='300px')
+      Eplot(type="line",data=data_line_bar)
     })
     output$data_line_bar_2 <- renderEplot({
       Eplot(type="bar",data=data_line_bar_2)
@@ -36,7 +36,7 @@ shinyServer(
     output$data_funnel <- renderEplot({
       Eplot(type="funnel",data=data_funnel)
     })
-    
+
     output$data_map_china <- renderEplot({
       Eplot(type="map",data=data_map_china,
             mapType="china",visualMap_max=5000
